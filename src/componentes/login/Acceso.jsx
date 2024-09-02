fb80495435eae224264c18aaebb42e260c6abb0e
bricio <briciopincha@gmail.com>
2024-09-02 17:10:28 -0300

    a

--- src/componentes/login/Acceso.jsx
+++ src/componentes/login/Acceso.jsx
@@ -7,7 +7,7 @@ import "./estilo/Acceso.css";
 
 function Acceso() {
     return (
-        <div>
+        <div> 
             <Nav />
             <div className='d-flex align-items-center justify-content-center gap-5 my-5'>
                 <div>
