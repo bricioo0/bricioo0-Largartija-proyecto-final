72838100928ec3c215fca9c53f551df50adfecb0
bricio <briciopincha@gmail.com>
2024-10-31 15:11:14 -0300

    jazmin

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,8 @@ import Registrarse from './componentes/login/Registrarse';
 import Home from './componentes/paginas/Home';
 import RecuperarContrasena from './componentes/login/RecuperarContrasena';
 import Remeras from './componentes/paginas/Remeras';
-
+import Buzos from  './componentes/paginas/Buzos'
+import Pantalones from './componentes/paginas/Pantalones';
 import Zapatillas from './componentes/paginas/zapatillas'
 import Carrito from  './componentes/paginas/Carrito';
 import ProductManagement from './componentes/paginas/productMangament';
@@ -28,8 +29,8 @@ function App() {
         <Route path="/" element={<Registrarse />} />
         <Route path="/recuperar-contrasena" element={<RecuperarContrasena />} />
         <Route path="/remeras" element={<Remeras />} />
-       
-
+        <Route path="/buzos" element={<Buzos />} />
+        <Route path="/pantalones" element={<Pantalones/>} />
         <Route path="/zapatillas" element={<Zapatillas/>} />
         <Route path="/carrito" element={<Carrito/>} />
         <Route path="/productMangament" element={<ProductManagement/>} />
